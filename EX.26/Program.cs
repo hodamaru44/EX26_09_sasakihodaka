82859ed4b3d427a36f6402e0bb916921dd702a33
sskho <sskho@DESKTOP-9UBA2DQ>
2021-11-17 10:13:52 +0900

    プロジェクト ファイルを追加します。

---
+++ EX.26/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+
+namespace EX._26
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            float Length = GetValue("幅を決めてください");
+            float Height = GetValue("高さを決めてください");
+            Triangul triangul1 = new Triangul(Length, Height);
+            Console.WriteLine($"体積は{triangul1.GetSurface()}");
+        }
+        static float GetValue(string nema)
+        {
+            float value;
+            Console.Write(nema+":") ;
+            value = float.Parse(Console.ReadLine());
+            return value;
+        }
+    }
+}
+
+class Triangul
+{
+    float Length;
+    float Height;
+
+    public Triangul(float l,float h)
+    {
+       this.Length = l;
+       this.Height = h;
+    }
+
+   public float GetSurface()
+    {
+        return (Length*Height/2);
+    }
+
+}
